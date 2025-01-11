b86eb9c66d6211ef0e3c7ed9910e6bed8729d9cc
joaquinbogado <joaquinbogado@gmail.com>
2025-01-11 16:17:39 -0300

    Agregadas visualizaciones estatica e interactiva

--- michigrad/visualize.py
+++ michigrad/visualize.py
@@ -1,10 +1,11 @@
 import networkx as nx
 import pyvis
 from pyvis.network import Network
+from graphviz import Digraph
 
 from .engine import Value
 
-def show_graph(self, filename="graph.html"):
+def show_graph_interactive(self, filename="graph.html"):
     nodes = {} # Diccionario para almacenar nodos por ID
     edges = []
 
@@ -33,3 +34,35 @@ def show_graph(self, filename="graph.html"):
     net.from_nx(graph)
     net.prep_notebook()
     net.show(filename)
+
+def trace(root):
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def show_graph(root, format='svg', rankdir='LR'):
+    """
+    format: png | svg | ...
+    rankdir: TB (top to bottom graph) | LR (left to right)
+    """
+    assert rankdir in ['LR', 'TB']
+    nodes, edges = trace(root)
+    dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
+    
+    for n in nodes:
+        dot.node(name=str(id(n)), label = "{%s | data %.4f | grad %.4f }" % (n.name, n.data, n.grad), shape='record')
+        if n._op:
+            dot.node(name=str(id(n)) + n._op, label=n._op)
+            dot.edge(str(id(n)) + n._op, str(id(n)))
+    
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+    
+    return dot
+
